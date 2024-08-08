ac745ac08fbcd5bbf9f9759b2cfd811f072e4fcc
FIRSTNAME LASTNAME <YOUR@EMAIL.COM>
2024-08-08 09:43:17 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const enterDOM = document.querySelector('.enter');
+const btnDOM = document.querySelector('.btn');
+const searchDOM = document.getElementById('search');
+const listDOM = document.getElementById('list');
